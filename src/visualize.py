bc5c12a9134313a3a12b6108e0a0511f1caf3737
Rishi Malhotra <rishi.malhotra920@gmail.com>
2024-06-21 22:51:35 -0700

    fixing visualizer

--- src/visualize.py
+++ src/visualize.py
@@ -1,10 +1,13 @@
-from utils import display_random_images, predict_on_random_images
-from torchvision import transforms
-import yaml
-from data_setup import create_datasets
-from run_manager import load_checkpoint
-import model_builder
 import argparse
+
+import torch
+import yaml
+from data_setup import yolo_pretrain_data_setup
+from models import yolo_net
+from run_manager import load_checkpoint
+from torchvision import transforms
+from utils import display_random_images, predict_on_random_images
+
 config = yaml.safe_load(open("config.yaml"))
 
 
@@ -17,8 +20,8 @@ def visualize(args):
         transforms.RandomRotation(30),
         transforms.ColorJitter(brightness=0.5, contrast=0.5),
         transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-        #  std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
         # transforms.RandomErasing()
     ])
 
@@ -29,7 +32,7 @@ def visualize(args):
     classes = ["n01986214", "n02009912", "n01924916"]
     # class_names = get_class_names_from_folder_names(classes)
 
-    train_dataset, mini_val_dataset = create_datasets(
+    train_dataset, mini_val_dataset = yolo_pretrain_data_setup.create_datasets(
         config["image_net_data_dir"],
         data_transform)
 
@@ -38,9 +41,10 @@ def visualize(args):
     # output_shape=len(classes)
     # ).to("cpu")
 
-    model = model_builder.DeepConvNet(dropout=0).to("cpu")
+    model = yolo_net.YOLOPretrainNet(dropout=0).to("cpu")
+    model = torch.nn.DataParallel(model)
 
-    load_checkpoint(model, args.run_id, args.checkpoint_path)
+    load_checkpoint(model, args.checkpoint_signature)
 
     predict_on_random_images(model, mini_val_dataset, n=10, seed=420)
 
@@ -50,15 +54,15 @@ def visualize(args):
 
 if __name__ == "__main__":
     # for example
-    # python visualize.py --run_id "IM-28" --checkpoint_path checkpoints/epoch_1
+    # python visualize.py --checkpoint_signature=IM-122:checkpoints/epoch_14
+
     # --hidden_units 256
     parser = argparse.ArgumentParser(
         description="Visualize the model's predictions")
     # parser.add_argument("--hidden_units", type=int,
     # help="The number of hidden units", required=True)
-    parser.add_argument("--run_id", type=str,
-                        help="The id of the run", required=True)
-    parser.add_argument("--checkpoint_path", type=str,
-                        help="The path to the checkpoint", required=True)
+
+    parser.add_argument("--checkpoint_signature", type=str,
+                        help="The signature for the checkpoint", required=True)
     args = parser.parse_args()
     visualize(args)
